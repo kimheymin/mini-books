88acfa2c39660b41f9fdde976f38c42b04fcd4cb
kimheymin <keg9317@naver.com>
2023-11-12 19:11:40 +0900

    검색 옵션 추가

--- src/pages/BookList.jsx
+++ src/pages/BookList.jsx
@@ -6,35 +6,6 @@ import { useParams } from "react-router";
 import Book from "../component/Book";
 
 export default function BookList() {
-  const { keyword } = useParams();
-
-  const { isLoading, error, data } = useQuery([keyword], async () => {
-    return await axios.get("/datas/data.json").then((res) => {
-      res.data.items.map((item) => {
-        item.author = item.author.replaceAll("^", " ");
-        item.publisher = item.publisher.replaceAll("^", " ");
-      });
-      return res.data.items;
-    });
-
-    //     return await axios({
-    //   method: "get",
-    //   baseURL: "/v1/search/book.json",
-    //   params: {
-    //     display: 100,
-    //     start: 1,
-    //     query: keyword,
-    //   },
-    //   headers: {
-    //     'X-Naver-Client-Id': `${process.env.REACT_APP_NAVER_API_KEY}`,
-    //     'X-Naver-Client-Secret': `${process.env.REACT_APP_NAVER_API_SECRET}`,
-    //   },
-    // })
-    // .then(res => res.data.items);
-  });
-
-  console.log(data);
-
   const showOptions = [
     { value: "10", name: "10개씩 보기" },
     { value: "30", name: "30개씩 보기" },
@@ -42,12 +13,50 @@ export default function BookList() {
   ];
 
   const searchOptions = [
-    { id: "recent", name: "option", text: "최신순" },
-    { id: "correct", name: "option", text: "정확도순" },
+    { id: "date", name: "option", text: "출간일순" },
+    { id: "sim", name: "option", text: "정확도순" },
   ];
 
+  const { keyword } = useParams();
+  const [searhOption, setSearhOption] = useState("sim");
+  const [showOption, setShowOption] = useState("");
+
+  const handleSearchfilterChange = (e) => setSearhOption(e.target.id);
   const handleShowCountChange = (e) => console.log(e.target.value);
-  const handleSearchfilterChange = (e) => console.log(e.target.id);
+
+  const { isLoading, error, data } = useQuery(
+    [keyword, searhOption],
+    async () => {
+      return await axios.get("/datas/data.json").then((res) => {
+        res.data.items.map((item) => {
+          item.author = item.author.replaceAll("^", " ");
+          item.publisher = item.publisher.replaceAll("^", " ");
+        });
+        return res.data.items;
+      });
+
+    //       return await axios({
+    //     method: "get",
+    //     baseURL: "/v1/search/book.json",
+    //     params: {
+    //       query: keyword,
+    //       display: 100,
+    //       start: 1,
+    //       sort : searhOption
+    //     },
+    //     headers: {
+    //       'X-Naver-Client-Id': `${process.env.REACT_APP_NAVER_API_KEY}`,
+    //       'X-Naver-Client-Secret': `${process.env.REACT_APP_NAVER_API_SECRET}`,
+    //     },
+    //   })
+    //   .then(res => {
+    //       res.data.items.map((item) => {
+    //         item.author = item.author.replaceAll("^", " ");
+    //         item.publisher = item.publisher.replaceAll("^", " ");
+    //       });
+    //       return res.data.items;
+    // })
+    });
 
   return (
     <section className="pt-10">
@@ -56,17 +65,29 @@ export default function BookList() {
           {searchOptions.map((item, index) => (
             <div className="pr-8 text-lg">
               <label key={index} onChange={handleSearchfilterChange}>
-                <input className="m-2
-                " type="radio" id={item.id} name={item.name} />
+                <input
+                  className="m-2"
+                  type="radio"
+                  id={item.id}
+                  name={item.name}
+                  defaultChecked={item.id === "sim"}
+                />
                 {item.text}
               </label>
             </div>
           ))}
         </div>
         <div className="">
-          <select className="bg-zinc-500 w-32 h-8 p-2 rounded-md" onChange={handleShowCountChange}>
+          <select
+            className="bg-zinc-500 w-32 h-8 p-2 rounded-md"
+            onChange={handleShowCountChange}
+          >
             {showOptions.map((item, index) => (
-              <option key={index} value={item.value} defaultValue={item.value}>
+              <option
+                key={index}
+                value={item.value}
+                defaultValue={item.value === "10"}
+              >
                 {item.name}
               </option>
             ))}
@@ -77,8 +98,7 @@ export default function BookList() {
       {data && (
         <div className="flex items-center text-2xl pt-8">
           <span className="font-semibold m-2 text-red-400">'{keyword}'</span> 에 대한
-          <span className="font-semibold m-2 text-red-400">{data.length}</span> 개의
-          검색결과
+          <span className="font-semibold m-2 text-red-400">{data.length}</span> 개의 검색결과
         </div>
       )}
 
