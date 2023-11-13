c55a01edec05ffc5d9fda42f52128f324e24d045
hm <keg9317@naver.com>
2023-11-14 00:28:20 +0900

    다크모드 추가

--- src/component/SearchHeader.jsx
+++ src/component/SearchHeader.jsx
@@ -1,15 +1,18 @@
-import React, { useEffect, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router";
 import { Link } from "react-router-dom";
 import { BiSolidSun, BiSolidMoon } from "react-icons/bi";
 import { FaSearch } from "react-icons/fa";
 import { CiDark } from "react-icons/ci";
+import { DarkModeContext } from "../context/DarkModeContext";
 
 export default function SearchHeader() {
   const navigate = useNavigate();
   const { keyword } = useParams();
   const [text, setText] = useState("");
-  const [darkMode, setDarkMode] = useState("");
+
+  const { darkMode, toggleDarkMode } = useContext(DarkModeContext);
+  const handleDarkMode = () => toggleDarkMode();
 
   const handleChange = (e) => setText(e.target.value);
   const handleSubmit = (e) => {
@@ -28,20 +31,20 @@ export default function SearchHeader() {
       <form className="w-full relative" onSubmit={handleSubmit}>
         <label htmlFor="text">
           <input
-            className="text-black pl-10 w-2/5 h-12 rounded-2xl text-lg font-bold"
+            className="pl-10 w-2/5 h-12 rounded-2xl text-lg font-bold"
             type="text"
             value={text}
             onChange={handleChange}
           />
         </label>
-        <button className="absolute top-4 left-2 text-black">
+        <button className="absolute top-4 left-2 ">
           <FaSearch />
         </button>
       </form>
 
       <div className="flex justify-between w-44">
         <Link to="/book/like">내 서재</Link>
-        <button className="pr-8 text-lg">
+        <button onClick={handleDarkMode} className="pr-8 text-lg">
           {!darkMode && <BiSolidMoon />}
           {darkMode && <BiSolidSun style={{ color: "white" }} />}
         </button>
